850f6add1c2b7387a8ec71e5a98f6997ed88d3d7
Chef NoName <abdijama21@gmail.com>
2021-08-17 01:58:43 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import store from "./store";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
